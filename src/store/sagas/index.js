fe170d3b1d937ef959f483f84179bc5c593576e4
Lucas Wundervald <wundervald_2@hotmail.com>
2019-02-13 18:32:41 -0200

    remove eg.

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -34,16 +34,5 @@ export default function* rootSaga() {
     takeLatest(CardTypes.UPDATE_CARD_REQUEST, updateCard),
     takeLatest(CardTypes.DELETE_CARD_REQUEST, deleteCard),
 
-    // takeLatest(TeamsTypes.GET_TEAMS_REQUEST, getTeams),
-    // // takeLatest(TeamsTypes.CREATE_TEAM_REQUEST, createTeam),
-
-    // takeLatest(TeamsTypes.SELECT_TEAM, getProjects),
-    // takeLatest(TeamsTypes.SELECT_TEAM, getPermissions),
-    // takeLatest(ProjectsTypes.GET_PROJECTS_REQUEST, getProjects),
-    // takeLatest(ProjectsTypes.CREATE_PROJECT_REQUEST, createProject),
-
-    // takeLatest(MembersTypes.GET_MEMBERS_REQUEST, getMembers),
-    // takeLatest(MembersTypes.UPDATE_MEMBER_REQUEST, updateMember),
-    // takeLatest(MembersTypes.INVITE_MEMBER_REQUEST, inviteMember),
   ]);
 }
